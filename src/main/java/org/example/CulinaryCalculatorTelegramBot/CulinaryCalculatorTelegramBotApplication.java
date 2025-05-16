4ee0c8df4ff649a5bd3da016160b053c81631927
Andrii Dmytrenko <dmitrenko1567@gmail.com>
2025-05-16 18:17:42 +0200

    +f

---
+++ src/main/java/org/example/CulinaryCalculatorTelegramBot/CulinaryCalculatorTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.CulinaryCalculatorTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryCalculatorTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryCalculatorTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/CulinaryCalculatorTelegramBot/CulinaryCalculatorTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+//package org.example.CulinaryCalculatorTelegramBot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CulinaryCalculatorTelegramBotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
